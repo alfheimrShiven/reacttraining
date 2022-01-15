165878d75db4bb385a0b6617b24dee5b88f38966
Shivendra Singh <shiven@Shivendras-MacBook-Pro.local>
2022-01-15 21:00:55 +0530

    Completed tictactoe with timetravel

--- src/index.js
+++ src/index.js
@@ -84,11 +84,12 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0,
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -100,7 +101,15 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext, 
+    });
+  }
+
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
@@ -108,8 +117,21 @@ class Game extends React.Component {
 
     // figuring out the latest state of the game
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    // showing moves history
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move :
+      'Go to game start';
+
+      return (<li key={move}>
+        <button onClick={() => this.jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+      );
+    });
 
     let status;
     if(winner)
@@ -128,7 +150,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
